596c56bb83d451b6357a13cde38e80029bfabfb8
Marcus Rugger <dev@marcusrugger.com>
2024-03-24 01:47:59 -0400

    telescope 0.1.6

--- lua/plug.lua
+++ lua/plug.lua
@@ -15,7 +15,7 @@ return require('packer').startup(function(use)
                     opt = true}
     }
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        'nvim-telescope/telescope.nvim', tag = '0.1.6',
         -- or                          , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
